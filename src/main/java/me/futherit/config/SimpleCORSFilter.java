90d504c4e5f05ef13fe1857d40c85aed1806b4ac
jucmoreno1 <51766518+jucmoreno1@users.noreply.github.com>
2020-05-25 15:41:32 -0500

    script git

--- src/main/java/me/futherit/config/SimpleCORSFilter.java
+++ src/main/java/me/futherit/config/SimpleCORSFilter.java
@@ -21,7 +21,8 @@ public class SimpleCORSFilter implements Filter {
         response.setHeader("Access-Control-Allow-Methods", "*");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me");
-
+        response.setHeader("Access-Control-Allow-Headers", "*");
+        
         chain.doFilter(req, res);
     }
 
